f5a243dbd1c615f94d3f713347cfaa18a67f03f6
Bunniebell <felipeccsantos1@gmail.com>
2023-11-23 22:44:50 -0300

    Cena Introdução FEITA
    titulo autoexplicativo

--- Assets/Scripts/Cutscene/CutsceneADM.cs
+++ Assets/Scripts/Cutscene/CutsceneADM.cs
@@ -1,45 +1,42 @@
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CutsceneADM : MonoBehaviour
 {
     [SerializeField]
-    private GameObject[] imagens;
-    //public TextMeshProUGUI[] cutsceneInitialText;
+    private Image[] imagens;
 
     private int imagemAtual = 0;
-    //private int cutsceneAtual = 0;
 
     void Start()
     {
         Application.targetFrameRate = 60;
 
-        Invoke("ChangeImage", 12f);
+        Invoke("TrocarImagens", 12f);
     }
 
     void Update()
     {
-        imagens[imagemAtual].GetComponent<SpriteRenderer>().color += new Color (0,0,0,0.01f);
-
-        //cutsceneInitialText[cutsceneAtual].color += new Color(0, 0, 0, 0.01f);
+        imagens[imagemAtual].color += new Color (0,0,0,0.01f);
     }
 
-    private void ChangeImage()
+    private void TrocarImagens()
     {
         if (imagemAtual < imagens.Length - 1)
         {
-            imagens[imagemAtual].SetActive(false);
-
-            imagens[imagemAtual + 1].SetActive(true);
+            imagens[imagemAtual].gameObject.SetActive(false);
+            
+            imagens[imagemAtual + 1].gameObject.SetActive(true);
 
             imagemAtual++;
         }
         else
         {
-            SceneManager.LoadScene("ApartamentoSofia 1");
+            SceneManager.LoadScene("Quingoma");
         }
 
-        Invoke("ChangeImage", 12f);
+        Invoke("TrocarImagens", 12f);
     }
 }
